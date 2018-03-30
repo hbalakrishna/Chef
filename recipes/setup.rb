17131577bef5183c54d7f368b97734782a757c4f
Cloud User <centos@ip-172-31-95-231.ec2.internal>
2018-03-30 16:43:34 +0000

    ohia

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -8,8 +8,17 @@ package 'ntp' do
   action:install
 end
 
+node_ip = node['ipaddress']
+#node['memory']['total']
+puts "Ip address of the node is #{node_ip} "
+
+
 file 'etc/motd' do
-  content 'This is the property of haxr'
+  content "This is the property of haxr
+  HostName : #{node['hostname']}
+  IPADDRESS: #{node_ip}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMPRY: #{node['memory']['total']}"
   owner 'root'
   group 'root'
 end
